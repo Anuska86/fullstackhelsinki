7cdf7f98cb2a3812d257e1f6a43d0ea71ffbaf65
Anuska86 <anasrm86@gmail.com>
2024-02-20 15:25:34 +0100

    Update App.jsx

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,9 +1,20 @@
 /* eslint-disable react/prop-types */
 /*data>>courseData>>response*/
 
+const Course = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <Header headerData={props.courseData.name} />
+      <Content contentData={props.courseData.parts} />
+      <Total totalExercises={props.courseData.parts} />
+    </div>
+  );
+};
+
 const Header = (props) => {
-  console.log('Header:')
-  console.log(props)
+  console.log("Header:");
+  console.log(props);
   return (
     <div>
       <h1>Title: {props.headerData}</h1>
@@ -11,32 +22,30 @@ const Header = (props) => {
   );
 };
 
-const Course = (props) => {
-  console.log('Course:')
-  console.log(props)
-  return (
-    <div>
-      <Header headerData={props.courseData.name} />
-      <Content ContentData={props.courseData.parts} />
-    </div>
-  );
-}
-
-
-
 const Content = (props) => {
+  console.log("Content:");
   console.log(props);
   return (
     <div>
-       <p>
-        {props.ContentData[0].name} {props.ContentData[0].exercises}
+      <p>
+        {props.contentData[0].name} {props.contentData[0].exercises}
       </p>
       <p>
-        {props.ContentData[1].name} {props.ContentData[1].exercises}
+        {props.contentData[1].name} {props.contentData[1].exercises}
       </p>
       <p>
-        {props.ContentData[2].name} {props.ContentData[2].exercises}
+        {props.contentData[2].name} {props.contentData[2].exercises}
       </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  let total = props.totalExercises.reduce((x, y) => (x = x + y.exercises), 0);
+  return (
+    <div>
+      <h1>The total of exercises is: {total}</h1>
+      <p></p>
     </div>
   );
 };
@@ -44,27 +53,27 @@ const Content = (props) => {
 const App = () => {
   const courseInfo = {
     id: 1,
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: "Fundamentals of React",
         exercises: 10,
-        id: 1
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
+        name: "Using props to pass data",
         exercises: 7,
-        id: 2
+        id: 2,
       },
       {
-        name: 'State of a component',
+        name: "State of a component",
         exercises: 14,
-        id: 3
-      }
-    ]
-  }
+        id: 3,
+      },
+    ],
+  };
 
-  return <Course courseData={courseInfo} />
-}
+  return <Course courseData={courseInfo} />;
+};
 
-export default App
+export default App;
