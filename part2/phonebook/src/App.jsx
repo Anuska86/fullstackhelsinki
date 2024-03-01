07273cc2a704d87f629965d7ee17ccb50d64f0f3
Anuska86 <anasrm86@gmail.com>
2024-03-01 11:30:06 +0100

    findContact

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,34 @@
 import { useState } from "react";
 import Person from "./components/Person";
 
-const App = (props) => {
-  const [persons, setPersons] = useState(props.persons);
+const App = () => {
+  // eslint-disable-next-line react/prop-types
+  const [persons, setPersons] = useState([
+    { id: 1, name: "Courteney Cox", phoneNumber: "212-4653326" },
+    { id: 2, name: "Jennifer Aniston", phoneNumber: "212-4659852" },
+    { id: 3, name: "Lisa Kudrow", phoneNumber: "212-4652226" },
+    { id: 4, name: "Matthew Perry", phoneNumber: "212-4743326" },
+    { id: 5, name: "David Schwimmer", phoneNumber: "212-4659826" },
+    { id: 6, name: "Matt LeBlanc", phoneNumber: "212-4653336" },
+  ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState();
+  const [findName, setFindName] = useState();
+  const [contactExistAlert, setcontactExistAlert] = useState();
 
   const addPerson = (event) => {
     event.preventDefault();
     let valueAlreadyExists = persons.find((x) => x.name == newName);
-    let numberAlreadyExists =persons.find((x)=>x.phoneNumber==newPhone);
+    let numberAlreadyExists = persons.find((x) => x.phoneNumber == newPhone);
     if (valueAlreadyExists) {
       alert(`${newName} already exists!`);
       return;
-    } else if (numberAlreadyExists){
-      alert(`${newPhone} already exists!`)
-    }
-    else {
+    } else if (numberAlreadyExists) {
+      alert(`${newPhone} already exists!`);
+    } else {
       const personObject = {
         name: newName,
-        phoneNumber:newPhone,
+        phoneNumber: newPhone,
         id: persons.length + 1,
       };
       setPersons(persons.concat(personObject));
@@ -36,9 +45,40 @@ const App = (props) => {
     setNewPhone(event.target.value);
   };
 
+  const findPerson = (event) => {
+    event.preventDefault();
+    let notExists = true; 
+    for(let i=0; i< persons.length;i++){
+      if(persons[i].name==findName){
+        setcontactExistAlert(JSON.stringify({ name: persons[i].name, phone: persons[i].phoneNumber }));
+        notExists = false;
+      }
+    }
+    if (notExists) {
+      setcontactExistAlert(`${findName} is not on the phonebook`);
+    }
+
+  };
+
+  const handleFindNameChange = (event) => {
+    setFindName(event.target.value);
+  };
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Phonebook:</h2>
+      <form onSubmit={findPerson}>
+        <div>
+          Find a contact:{" "}
+          <input value={findName} onChange={handleFindNameChange} />
+        </div>
+        <div>
+          <button type="submit">Find contact</button>
+          {contactExistAlert}
+        </div>
+      </form>
+
+      <h2>Add a new contact:</h2>
 
       <form onSubmit={addPerson}>
         <div>
@@ -55,7 +95,11 @@ const App = (props) => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <Person key={person.id} person={person} phoneNumber={person.phoneNumber} />
+          <Person
+            key={person.id}
+            person={person}
+            phoneNumber={person.phoneNumber}
+          />
         ))}
       </ul>
     </div>
--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -4,45 +4,6 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-const persons = [
-  {
-    id: 1,
-    name: 'Courteney Cox',
-    phoneNumber:'212-4653326'
-    
-  },
-  {
-    id: 2,
-    name: 'Jennifer Aniston',
-    phoneNumber:'212-4659852'
-    
-  },
-  {
-    id: 3,
-    name: 'Lisa Kudrow',
-    phoneNumber:'212-4652226'
-    
-  },
-  {
-    id: 4,
-    name: 'Matthew Perry',
-    phoneNumber:'212-4743326'
-    
-  },
-  {
-    id: 5,
-    name: 'David Schwimmer',
-    phoneNumber:'212-4659826'
-    
-  },
-  {
-    id: 6,
-    name: 'Matt LeBlanc',
-    phoneNumber:'212-4653336'
-    
-  }
-]
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App persons={persons} />
+  <App />
 )
