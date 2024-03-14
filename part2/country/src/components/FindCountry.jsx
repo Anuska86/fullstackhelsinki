e8f84a32c4c8b931b796404536d7384d5ba27f8c
Anuska86 <anasrm86@gmail.com>
2024-03-14 11:30:06 +0100

    Update

--- part2/country/src/components/FindCountry.jsx
+++ part2/country/src/components/FindCountry.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import countryService from "../services/countryService";
 
-const FindCountry = ({findCountryName,countriesList}) => {
+const FindCountry = ({ findCountryName, countriesList }) => {
   //const [findCountryName, setFindCountryName] = useState();
   const [countryExistAlert, setCountryExistAlert] = useState();
   const [tooManyElementsAlert, setTooManyElementsAlert] = useState();
   const [countryName, setCountryName] = useState();
+  const [countryCapital, setCountryCapital] = useState();
+  const [countryArea, setCountryArea] = useState();
   const [flag, setFlag] = useState();
   //const [countriesList, setCountries] = useState([]);
   const [numberOfMatches, setNumberOfMatches] = useState([]);
@@ -25,49 +27,60 @@ const FindCountry = ({findCountryName,countriesList}) => {
     setNumberOfMatches([]);
     let exist;
     for (let i = 0; i < countriesList.length; i++) {
-      let resultOfMatch = regexFindLetters(findCountryName, countriesList[i].name.common);
-      if(resultOfMatch!==null){
-          setNumberOfMatches((numberOfMatches) => [...numberOfMatches,resultOfMatch]);
+      let resultOfMatch = regexFindLetters(
+        findCountryName,
+        countriesList[i].name.common
+      );
+      if (resultOfMatch !== null) {
+        setNumberOfMatches((numberOfMatches) => [
+          ...numberOfMatches,
+          resultOfMatch,
+        ]);
       }
     }
-    if(numberOfMatches.length>10){
-      console.log(numberOfMatches)
-      setTooManyElementsAlert('Too many elements');
-    } else if (numberOfMatches.length===1) {
-      let selectedCountry = countriesList.find((c) => c.name.common === findCountryName);
-      let bd={
-        "name":selectedCountry.name.common,
-        "capital":selectedCountry.capital[0],
-        "area":selectedCountry.area,
-        "flag":selectedCountry.flags.png
+    if (numberOfMatches.length > 10) {
+      console.log(numberOfMatches);
+      setTooManyElementsAlert("Too many elements");
+    } else if (numberOfMatches.length === 1) {
+      let selectedCountry = countriesList.find(
+        (c) => c.name.common === findCountryName
+      );
+      let bd = {
+        name: selectedCountry.name.common,
+        capital: selectedCountry.capital[0],
+        area: selectedCountry.area,
+        flag: selectedCountry.flags.png,
       };
-      setFlag(bd.flag)
-      setCountryName(bd.name)
-      let d=bd.toString()
-      console.log(d)
-    } else if (numberOfMatches.length<=10){
-      let c=numberOfMatches.toString()
-      console.log(c)
+      setCountryName(bd.name);
+      setCountryCapital(bd.capital);
+      setCountryArea(bd.area);
+      setFlag(bd.flag);
+
+      let d = bd.toString();
+      console.log(d);
+    } else if (numberOfMatches.length <= 10) {
+      let c = numberOfMatches.toString();
+      console.log(c);
       setTooManyElementsAlert(c);
     }
     exist = countriesList.find((c) => c.name.common === findCountryName);
     if (!exist) {
       setCountryExistAlert(`${findCountryName} is not on the database`);
     }
-
   };
 
   return (
-
+    <div>
+      <button onClick={findCountry}>Find country</button>
+      {countryExistAlert}
+      <div>{tooManyElementsAlert}</div>
       <div>
-        <button onClick={findCountry}>Find country</button>
-        {countryExistAlert}
-        <div>{tooManyElementsAlert}</div>
-        <div>
-          {countryName}
-        </div>
-        <img src={flag} alt="" />
+        <li>{countryName}</li>
+        <li>{countryCapital}</li>
+        <li>{countryArea}</li>
       </div>
+      <img src={flag} alt="" />
+    </div>
   );
 };
 
